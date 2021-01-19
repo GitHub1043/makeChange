167d2d8d35b61b8a6f4c856df5a86a52558fed5c
AYDEN HOLGATE <AYDEN.HOLGATE@connect.7oaks.org>
2021-01-19 15:31:15 -0600

    Initial commit

---
+++ makeChange/Form1.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblCake = New System.Windows.Forms.Label()
+        Me.txtMoney = New System.Windows.Forms.TextBox()
+        Me.btnPay = New System.Windows.Forms.Button()
+        Me.lblMoney = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblCake
+        '
+        Me.lblCake.AutoSize = True
+        Me.lblCake.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCake.Location = New System.Drawing.Point(12, 9)
+        Me.lblCake.Name = "lblCake"
+        Me.lblCake.Size = New System.Drawing.Size(128, 16)
+        Me.lblCake.TabIndex = 1
+        Me.lblCake.Text = "Cost of Cake: $35.99"
+        '
+        'txtMoney
+        '
+        Me.txtMoney.Location = New System.Drawing.Point(237, 64)
+        Me.txtMoney.Name = "txtMoney"
+        Me.txtMoney.Size = New System.Drawing.Size(100, 20)
+        Me.txtMoney.TabIndex = 2
+        '
+        'btnPay
+        '
+        Me.btnPay.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPay.Location = New System.Drawing.Point(12, 125)
+        Me.btnPay.Name = "btnPay"
+        Me.btnPay.Size = New System.Drawing.Size(75, 26)
+        Me.btnPay.TabIndex = 3
+        Me.btnPay.Text = "Pay"
+        Me.btnPay.UseVisualStyleBackColor = True
+        '
+        'lblMoney
+        '
+        Me.lblMoney.AutoSize = True
+        Me.lblMoney.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMoney.Location = New System.Drawing.Point(12, 64)
+        Me.lblMoney.Name = "lblMoney"
+        Me.lblMoney.Size = New System.Drawing.Size(205, 16)
+        Me.lblMoney.TabIndex = 4
+        Me.lblMoney.Text = "How Much Money Do You Have?"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(398, 191)
+        Me.Controls.Add(Me.lblMoney)
+        Me.Controls.Add(Me.btnPay)
+        Me.Controls.Add(Me.txtMoney)
+        Me.Controls.Add(Me.lblCake)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblCake As System.Windows.Forms.Label
+    Friend WithEvents txtMoney As System.Windows.Forms.TextBox
+    Friend WithEvents btnPay As System.Windows.Forms.Button
+    Friend WithEvents lblMoney As System.Windows.Forms.Label
+
+End Class
---
+++ makeChange/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Dim sngChange As Single
+   
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnPay.Click
+        Const CAKE As Single = 35.99
+        Dim sngMoney As Single
+
+        sngMoney = Val(txtMoney.Text)
+
+        If (sngMoney = CAKE) Then
+            MessageBox.Show("You have exact change")
+
+        ElseIf (sngMoney < CAKE) Then
+            MessageBox.Show("You don't have enough")
+
+        ElseIf (sngMoney > CAKE) Then
+            sngChange = sngMoney - CAKE
+            MessageBox.Show("Change: $" & sngChange)
+        End If
+
+        If (sngMoney > CAKE * 2) Then
+            MessageBox.Show("You have more than double!")
+        End If
+
+    End Sub
+End Class
